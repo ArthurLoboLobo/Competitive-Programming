225b3170213ebaae2f8b993609c1bd21c2148773
Arthur Lobo <122179618+ArthurLoboLobo@users.noreply.github.com>
2025-09-12 10:51:38 -0300

    Update Segment Tree - Sparse.cpp

--- library/data structures/SegTree's/Segment Tree - Sparse.cpp
+++ library/data structures/SegTree's/Segment Tree - Sparse.cpp
@@ -1,48 +1,63 @@
 /*
 Sparse Segment Tree
-(Não terminei ainda - parei no upd)
-minv,maxv -> Node 1 interval
-start -> build(1,minv,maxv);
-update position "pos" with "val" -> upd(1,minv,maxv,pos,val);
-query on range L,R -> qry(1,minv,maxv,L,R);
+Status: Not tested yet
+start -> segtree(minv,maxv) - (minv,maxv) = node 1 interval
+update position "pos" with "val" -> update(pos,val);
+query on range l,r -> query(l,r)
 */
 
 struct segtree {
+    vector</* type */> tr;
+    vector<int> lc,rc;
+    int cntnode, minv, maxv;
 
-	/* type */ tr[/* size */];
-	int lc[/* size */], rc[/* size */];
-	int cntnode = 0;
+    segtree(int minv_, int maxv_) {
+        minv = minv_;
+        maxv = maxv_;
+        cntnode = 1;
+        tr.assign(2,/* neutral value */);
+        lc.assign(2,0);
+        rc.assign(2,0);
+    }
 
-	/* type */ mergeseg(/* type */ valL, /* type */ valR) {
-		
-		/* return merge of valL and valR */
-	}
+    /* type */ mergeseg(/* type */ valL, /* type */ valR) {
+        
+        /* return merge of valL and valR */
+    }
 
     int upd(int no, int l, int r, int pos, /* type */ val) {
         if(l > pos or r < pos) return no;
 
-		if(no == 0) {
-			no = ++cntno;
-			lc[no] = 0;
-			rc[no] = 0;
-			tr[no] = /* neutral value */;
-		}
-		
+        if(no == 0) {
+            no = ++cntno;
+            tr.pb(/* neutral value */);
+            lc.pb(0);
+            rc.pb(0);
+        }
+        
         if(l == r) {
             tr[no] = /* adds val to tr[no] */;
             return no;
         }
         int mid=(l+r)>>1;
-		lc[no] = upd(lc[no],l,mid,pos,val);
-		rc[no] = upd(rc[no],mid+1,r,pos,val);
+        lc[no] = upd(lc[no],l,mid,pos,val);
+        rc[no] = upd(rc[no],mid+1,r,pos,val);
         tr[no] = mergeseg(tr[lc],tr[rc]);
-		return no;
+        return no;
     }
 
     /* type */ qry(int no, int l, int r, int ll, int rr) {
-        if(l > rr or r < ll) return /* neutral value */;
+        if(no == 0 or l > rr or r < ll) return /* neutral value */;
         if(l >= ll and r <= rr) return tr[no];
-        int lc=2*no,rc=2*no+1,mid=(l+r)>>1;
-        return mergeseg(qry(lc,l,mid,ll,rr),qry(rc,mid+1,r,ll,rr));
+        int mid=(l+r)>>1;
+        return mergeseg(qry(lc[no],l,mid,ll,rr),qry(rc[no],mid+1,r,ll,rr));
+    }
+
+    void update(int pos, /* type */ val) {
+        upd(1,minv,maxv,pos,val);
+    }
+
+    /* type */ query(int l, int r) {
+        return qry(1,minv,maxv,l,r);
     }
 };
